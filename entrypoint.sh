e754b94c1676fbd528f1c70662e75ec874a4fbef
Roman Timushev <rtimush@gmail.com>
2019-09-30 23:23:49 +0200

    Wrap shell variables in double quotes

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,10 @@ set -euo pipefail
 case "$1" in
     propagate)
         tenpureto template propagate-changes \
-            --template https://github.com/${GITHUB_REPOSITORY}.git  \
-            --branch ${GITHUB_REF#refs/heads/} \
+            --template "https://github.com/${GITHUB_REPOSITORY}.git"  \
+            --branch "${GITHUB_REF#refs/heads/}" \
             --pull-request \
             $([[ -z "${PULL_REQUEST_LABEL}" ]] && true || echo "--pull-request-label ${PULL_REQUEST_LABEL}") \
-            --pull-request-assignee ${GITHUB_ACTOR} \
+            --pull-request-assignee "${GITHUB_ACTOR}" \
             --unattended
 esac
